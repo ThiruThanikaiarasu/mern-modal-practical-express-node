bbd2e37049a897034f922e4cc3032d591e787478
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-06-12 13:50:42 +0530

    Add. Included employee router and DB connection Established.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const PORT = 3500
+const employee = require('./routers/employeeRouter')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+
+db.on("error",(error)=> console.log(error.message))
+db.once("open",()=> console.log("Connection Established"))
 
 app.get('/',(request,response)=>{
     response.send('working')
 })
+
+app.use('/api/v1/employee',employee)
 
 app.listen(PORT,console.log(`Server is running at http://localhost:${PORT}`))
